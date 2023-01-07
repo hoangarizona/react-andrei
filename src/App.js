4c0ab4ae355766edcd2790b96a5d24ea166eeb7e
Thanh Nguyen <hoangarizona@gmail.com>
2023-01-06 17:30:40 -0700

    optimize event handler as a method of the app object instead of anonymous method

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-    constructor() {//run 1st (1st life cycle)
-        super();
-        this.state = {
-            monsters: [],//initial value
-            searchField: ''
-        }
-    }
-    componentDidMount() { //run 3rd (3rd life cycle) and move on to render because state changes
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState(() => {
-                return { monsters: users }
-            },
-                () => { console.log(this.state) }
-            ))
-    }
-    render() {//run 2nd (2nd life cycle)
-
-        const filteredMonsters = this.state.monsters.filter((monster) => { //IMMUTABILITY BECAUSE FILTER CREATE A NEW ONE
-            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-        });
-
-        return (
-            <div className="App">
-                <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-                    // [{name:}]
-                    const searchField = event.target.value.toLocaleLowerCase();
-
-                    this.setState(() => {
-                        return { searchField };//shortcut because of key and value are the same
-                    });
-                }}
-                ></input>
-
-                {filteredMonsters.map(
-                    (monster) => {
-                        return <div key={monster.id}><h1>{monster.name}</h1></div>;
-                    }
-                )
-                }
-            </div>
+  constructor() {
+    //run 1st (1st life cycle)
+    super();
+    this.state = {
+      monsters: [], //initial value
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    //run 3rd (3rd life cycle) and move on to render because state changes
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
         )
-    }
+      );
+  }
+  onSearchChange = (event) => {
+    // [{name:}]
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField }; //shortcut because of key and value are the same
+    });
+  };
+  render() {
+    //run 2nd (2nd life cycle)
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      //IMMUTABILITY BECAUSE FILTER CREATE A NEW ONE
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={this.onSearchChange}
+        ></input>
+
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-   
+
 export default App;
